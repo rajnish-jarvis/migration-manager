5245d242f8c86475131c0342b2d85ab14d5d3c75
rajnishmishra <rajnish.mishra@jarvis.consulting>
2025-02-16 15:44:52 +0530

    listing migrations

--- app/controllers/migration_manager/application_controller.rb
+++ app/controllers/migration_manager/application_controller.rb
@@ -1,4 +1,13 @@
 module MigrationManager
   class ApplicationController < ActionController::Base
+    before_action :authenticate_user
+
+    private
+
+    def authenticate_user
+      authenticate_or_request_with_http_basic do |user, password|
+        user == ENV['MIGRATION_MANAGER_USER'] && password == ENV['MIGRATION_MANAGER_PASSWORD']
+      end
+    end
   end
 end
