be170cd7004aa91f828f2179e6748f220e9a67f4
rajnishmishra <rajnish.mishra@jarvis.consulting>
2025-02-16 16:49:03 +0530

    listing migrations

--- lib/migration_manager.rb
+++ lib/migration_manager.rb
@@ -1,6 +1,5 @@
 require "rails"
 require "migration_manager/engine"
-require "migration_manager/middleware/skip_migration_check"
 module MigrationManager
   # Your module logic here
 end
--- lib/migration_manager/engine.rb
+++ lib/migration_manager/engine.rb
@@ -4,11 +4,5 @@ require "rails/engine"
 module MigrationManager
   class Engine < ::Rails::Engine
     isolate_namespace MigrationManager
-    initializer "migration_manager.skip_migration_check" do |app|
-      app.config.middleware.insert_before(
-        ActiveRecord::Migration::CheckPending,
-        MigrationManager::Middleware::SkipMigrationCheck
-      )
-    end
   end
 end
