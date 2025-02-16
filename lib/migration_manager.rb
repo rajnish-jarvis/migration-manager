e4fba70b86b06af99f06ba9c76ea279f0b2227f6
rajnishmishra <rajnish.mishra@jarvis.consulting>
2025-02-16 13:27:40 +0530

    testing

--- lib/migration_manager.rb
+++ lib/migration_manager.rb
@@ -1,3 +1,4 @@
+require "rails"
 require "migration_manager/engine"
 
 module MigrationManager
--- lib/migration_manager/engine.rb
+++ lib/migration_manager/engine.rb
@@ -1,3 +1,6 @@
+require "rails"
+require "rails/engine"
+
 module MigrationManager
   class Engine < ::Rails::Engine
     isolate_namespace MigrationManager
