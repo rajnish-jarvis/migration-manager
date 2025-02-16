1d546afb242281be582761f18b6a5003941c2514
rajnishmishra <rajnish.mishra@jarvis.consulting>
2025-02-16 17:14:56 +0530

    listing migrations

--- lib/migration_manager/engine.rb
+++ lib/migration_manager/engine.rb
@@ -4,10 +4,5 @@ require "rails/engine"
 module MigrationManager
   class Engine < ::Rails::Engine
     isolate_namespace MigrationManager
-    config.after_initialize do
-      ActiveSupport.on_load(:after_initialize) do
-        Rails.application.config.middleware.delete ActiveRecord::Migration::CheckPending
-      end
-    end
   end
 end
