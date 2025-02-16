bb5d6ffb8ed530194c4f1b317f8261811ec7f033
rajnishmishra <rajnish.mishra@jarvis.consulting>
2025-02-16 17:10:34 +0530

    listing migrations

--- app/controllers/migration_manager/home_controller.rb
+++ app/controllers/migration_manager/home_controller.rb
@@ -1,15 +1,8 @@
 module MigrationManager
   class HomeController < MigrationManager::ApplicationController
     def index
-      # Get all migrations and their statuses
-      @migrations = ActiveRecord::Base.connection.execute(
-        "SELECT version FROM schema_migrations"
-      ).map { |row| row["version"] }.sort.reverse # Sort DESC
-
-      @all_migrations = ActiveRecord::Migrator.migrations_paths.flat_map do |path|
-        ActiveRecord::MigrationContext.new(path).migrations
-      end.sort_by(&:version).reverse
-
+      @migrations = fetch_applied_migrations
+      @all_migrations = fetch_all_migrations
       @pending_migrations = @all_migrations.reject { |m| @migrations.include?(m.version.to_s) }
 
       @success_message = params[:success] if params[:success].present?
@@ -20,10 +13,26 @@ module MigrationManager
 
       if version.present?
         ActiveRecord::Migrator.run(:up, ActiveRecord::Migrator.migrations_paths, version.to_i)
-        redirect_to "#{request.base_url}/migration_manager/home", notice: "Migration #{version} successfully run.!"
+        redirect_to "#{request.base_url}/migration_manager/home", notice: "Migration #{version} successfully run!"
       else
         redirect_to "#{request.base_url}/migration_manager/home", alert: "Invalid migration version."
       end
+    end
+
+    private
+
+    def fetch_applied_migrations
+      ActiveRecord::Base.connection.select_values("SELECT version FROM schema_migrations").map(&:to_s)
+    rescue ActiveRecord::StatementInvalid, ActiveRecord::NoDatabaseError
+      []
+    end
+
+    def fetch_all_migrations
+      ActiveRecord::Migrator.migrations_paths.flat_map do |path|
+        ActiveRecord::MigrationContext.new(path).migrations
+      end.sort_by(&:version).reverse
+    rescue ActiveRecord::StatementInvalid, ActiveRecord::NoDatabaseError
+      []
     end
   end
 end
--- lib/migration_manager/engine.rb
+++ lib/migration_manager/engine.rb
@@ -4,5 +4,10 @@ require "rails/engine"
 module MigrationManager
   class Engine < ::Rails::Engine
     isolate_namespace MigrationManager
+    config.after_initialize do
+      ActiveSupport.on_load(:after_initialize) do
+        Rails.application.config.middleware.delete ActiveRecord::Migration::CheckPending
+      end
+    end
   end
 end
